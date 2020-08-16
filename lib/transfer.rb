54b3dab5a7840946603e9236466fc2340f363f40
Matt Wright <mttwright@gmail.com>
2020-08-16 15:40:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,22 +14,15 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.status == "open"
-      
+    if self.valid?
       if @sender.balance < @amount
+        "Transaction rejected.  Check account funds."
         @status = "rejected"
-        "Transaction rejected.  Please check your account balance."
-      elsif @status == "pending"
-        @sender.balance=(@sender.balance - @amount)
-        @receiver.balance=(@receiver.balance + @amount)
-        @status = "complete"
       else
-        @sender.balance
+        @sender.balance = @sender.balance - @amount
+        @receiver.balance = @receiver.balance - @amount
+        @status = "complete"
       end
-      
-    else
-      "Transaction rejected.  Please check your account balance."
     end
-  end
   
 end
