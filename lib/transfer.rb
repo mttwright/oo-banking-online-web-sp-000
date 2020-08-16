3c07894fb31937902d3608c8ca94a656fbe3c08b
Matt Wright <mttwright@gmail.com>
2020-08-16 15:51:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,8 @@ class Transfer
   def execute_transaction
     if self.valid?
       if @sender.balance < @amount
-        "Transaction rejected.  Check funds."
         @status = "rejected"
+        "Transaction rejected.  Check funds."
       else
         @sender.balance = @sender.balance - @amount
         @receiver.balance = @receiver.balance + @amount
